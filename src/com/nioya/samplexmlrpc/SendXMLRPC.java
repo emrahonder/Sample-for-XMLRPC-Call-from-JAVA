b25442b610d3606ddc069e0faac74ea93054448a
Emrah Onder <emrah.onder@ericsson.com>
2018-04-24 10:28:25 +0300

    Adding new class to get response and request as XML. Also there is a function to print xml pretty

--- src/com/nioya/samplexmlrpc/SendXMLRPC.java
+++ src/com/nioya/samplexmlrpc/SendXMLRPC.java
@@ -1,5 +1,7 @@
 package com.nioya.samplexmlrpc;
 
+import java.io.ByteArrayInputStream;
+import java.io.StringWriter;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
@@ -10,10 +12,28 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
 import org.apache.xmlrpc.XmlRpcException;
 import org.apache.xmlrpc.client.XmlRpcClient;
 import org.apache.xmlrpc.client.XmlRpcClientConfigImpl;
 import org.apache.xmlrpc.client.XmlRpcClientException;
+import org.apache.xmlrpc.client.XmlRpcTransport;
+import org.apache.xmlrpc.client.XmlRpcTransportFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+
 
 public class SendXMLRPC {
 	private String connectionURL = "";
@@ -84,7 +104,15 @@ public class SendXMLRPC {
 			config.setEnabledForExtensions(true);
 			client.setConfig(config);
 			generalInfoLogger("Config Settings Done");
+			MessageLoggingTransport loggingTransport = new MessageLoggingTransport(client);
+			final XmlRpcTransportFactory transportFactory = new XmlRpcTransportFactory()
+			{
+			    public XmlRpcTransport getTransport()
+			    {
 
+			        return loggingTransport;
+			    }
+			};
 			responseMap = (Map<String, Object>) client.execute("method:name", check_params);
 
 			if (responseMap == null) {
@@ -102,6 +130,12 @@ public class SendXMLRPC {
 				// DO SMT
 
 			}
+			
+			String request = loggingTransport.getRequest();
+			String response = loggingTransport.getResponse();
+			
+			generalInfoLogger("request" + prettyXML(request));
+			generalInfoLogger("response" +prettyXML(response));
 
 			generalInfoLogger("responseMap:" + responseMap.toString());
 
@@ -117,5 +151,45 @@ public class SendXMLRPC {
 	private void generalInfoLogger(String log) {
 		System.out.println("LOG:" + log);
 	}
+	
+	
+	 private String prettyXML(String xml){
+	    	return prettyXML(xml, 1);
+	    }
+	    private String prettyXML(String xml, int indent){
+	    	try {
+	            // Turn xml string into a document
+	            Document document = DocumentBuilderFactory.newInstance()
+	                    .newDocumentBuilder()
+	                    .parse(new InputSource(new ByteArrayInputStream(xml.getBytes("utf-8"))));
+
+	            // Remove whitespaces outside tags
+	            document.normalize();
+	            XPath xPath = XPathFactory.newInstance().newXPath();
+	            NodeList nodeList = (NodeList) xPath.evaluate("//text()[normalize-space()='']",
+	                                                          document,
+	                                                          XPathConstants.NODESET);
+
+	            for (int i = 0; i < nodeList.getLength(); ++i) {
+	                Node node = nodeList.item(i);
+	                node.getParentNode().removeChild(node);
+	            }
+
+	            // Setup pretty print options
+	            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	            transformerFactory.setAttribute("indent-number", indent);
+	            Transformer transformer = transformerFactory.newTransformer();
+	            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+	            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+	            transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+
+	            // Return pretty print xml string
+	            StringWriter stringWriter = new StringWriter();
+	            transformer.transform(new DOMSource(document), new StreamResult(stringWriter));
+	            return stringWriter.toString();
+	        } catch (Exception e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
 
 }
